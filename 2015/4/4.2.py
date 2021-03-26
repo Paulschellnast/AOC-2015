c59b4b7bb89e36d32c264e0198d30ea6f3257880
Paul Schellnast <paulschellnast@gmail.com>
2021-03-26 15:48:15 +0100

    4.2 puzzle

---
+++ 2015/4/4.2.py
@@ -0,0 +1,18 @@
+import sys 
+import hashlib 
+with open("2015/4/input.txt") as f:
+    content = f.read()
+    i = 1
+    while True:
+        m = hashlib.md5((content +str(i)).encode()).hexdigest()
+        if m[0] == "0" and m[1] == "0" and m[2] == "0" and m[3] == "0" and m[4] == "0" and m[5] == "0":
+            print(i)
+            sys.exit()
+
+            
+
+
+        
+        
+        
+        i += 1 
