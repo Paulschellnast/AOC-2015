fb8311e836291bf293e13b8c5c6a3d6db8f56e05
Paul Schellnast <paulschellnast@gmail.com>
2021-09-16 13:28:03 +0200

    final

---
+++ 2015/6/6.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+with open("2015/6/input.txt") as f:
+    content = f.read()
+    split = content.split()
+    start = 0
+    
+    
+
+    
+    
+
+    
+        
+        
+    
+        
+        
